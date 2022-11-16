d76290cc385e1af297bc5040aae72f3dbb0fc403
dmbrkgit <118228682+dmbrkgit@users.noreply.github.com>
2022-11-16 10:39:09 +0200

    Add files via upload

---
+++ Lab2/MainTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        int min = 1;
+        int max = 10;
+        assertEquals(11, min + max);
+    }
+}
